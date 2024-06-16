9567837dca3c796361711175ded41d5837e81170
jmcfdeveloper <jmcf.tic.developer@gmail.com>
2024-06-16 17:17:34 -0500

    Navbar component deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import TopNavbar from "@/components/navbar/blog_navbar";
 
 import TestNavbar from "@/components/navbar/testNavbar";
-import Navbar  from "@/components/navbar/Navbar";
+
 import { Sliders } from "@/components/sliders";
 import { Novedades } from "@/components/programa/novedades";
 import { Proximas } from "@/components/proximas";
